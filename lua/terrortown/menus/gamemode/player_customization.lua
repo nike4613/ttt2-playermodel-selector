7ff034342e493736c161658f59ce8e19a01905d6
DaNike <danike@cirr.com>
2026-01-17 20:34:17 -0600

    skeleton menu ui

---
+++ lua/terrortown/menus/gamemode/player_customization.lua
@@ -0,0 +1,7 @@
+--- @ignore
+
+CLGAMEMODEMENU.base = "base_gamemodemenu"
+CLGAMEMODEMENU.icon = Material("vgui/ttt/vskin/helpscreen/appearance")
+CLGAMEMODEMENU.title = "menu_customization_title"
+CLGAMEMODEMENU.description = "menu_customization_description"
+CLGAMEMODEMENU.priority = 97
---
+++ lua/terrortown/menus/gamemode/player_customization/selector.lua
@@ -0,0 +1,9 @@
+--- @ignore
+
+CLGAMEMODESUBMENU.base = "base_gamemodesubmenu"
+CLGAMEMODESUBMENU.priority = 99
+CLGAMEMODESUBMENU.title = "submenu_customization_selector"
+
+function CLGAMEMODESUBMENU:Populate(parent)
+    local form = vgui.CreateTTT2Form(parent, "header_customization_selector_form1")
+end
