bea7fcbc9255c0e088fd0ff124b63026ee361f0b
DaNike <danike@cirr.com>
2026-01-20 17:37:53 -0600

    Implement client/server sync routines for playermodel selector settings

--- lua/autorun/sh_ttt2pms.lua
+++ lua/autorun/sh_ttt2pms.lua
@@ -2,10 +2,12 @@ if SERVER then
     AddCSLuaFile()
     AddCSLuaFile("ttt2pms/types.lua")
     AddCSLuaFile("ttt2pms/sh_convars.lua")
+    AddCSLuaFile("ttt2pms/sh_plysettings.lua")
 end
 
 include("ttt2pms/types.lua")
 include("ttt2pms/sh_convars.lua")
+include("ttt2pms/sh_plysettings.lua")
 
 if SERVER then
     include("ttt2pms/sv_database.lua")
---
+++ lua/ttt2pms/sh_plysettings.lua
@@ -0,0 +1,77 @@
+util.AddNetworkString("TTT2PMS_SyncPlayerSettings")
+
+ttt2pms = ttt2pms or {}
+
+if SERVER then
+    ---
+    ---@param ply Player
+    ---@return PlayermodelSettings
+    ---@realm server
+    function ttt2pms.GetPlayerSettings(ply)
+        if not IsValid(ply) or type(ply) ~= "Player" then
+            error("ttt2pms.GetPlayerSettings() must take a player")
+        end
+
+        return ttt2pms.db.GetOptionsForPlayer(ply)
+    end
+
+    ---
+    ---@param ply Player|nil
+    ---@realm server
+    function ttt2pms.SyncPlayerSettings(ply)
+        if not ply then
+            -- if no player specified, explcit sync to all
+            Dev(1, "TTT2PMS: Explicit player settings sync-to-all triggered")
+            local plys = player.GetAll()
+            for i = 1, #plys do
+                ttt2pms.SyncPlayerSettings(plys[i])
+            end
+            return
+        end
+
+        net.SendStream("TTT2PMS_SyncPlayerSettings", ttt2pms.GetPlayerSettings(ply), ply)
+    end
+
+    hook.Add("PlayerInitialSpawn", "TTT2PMS_PlayerInitialSpawn", function(ply)
+        -- Player is spawning in, sync that player's settings to them
+        ttt2pms.SyncPlayerSettings(ply)
+    end)
+
+    net.ReceiveStream("TTT2PMS_SyncPlayerSettings", function(plySettings, ply)
+        -- client has sent us updated settings for them, store that
+        ttt2pms.db.SaveOptionsForPlayer(ply, plySettings)
+    end)
+end
+
+if CLIENT then
+    ---@type PlayermodelSettings
+    local playerSettings
+
+    ---
+    ---@return PlayermodelSettings
+    ---@realm client
+    function ttt2pms.GetPlayerSettings()
+        return playerSettings
+    end
+
+    ---@realm client
+    function ttt2pms.SyncPlayerSettings()
+        if not playerSettings then
+            return -- do nothing if we haven't received any settings from the server, nor set any ourselves
+        end
+
+        net.SendStream("TTT2PMS_SyncPlayerSettings", playerSettings)
+    end
+
+    ---
+    ---@param settings PlayermodelSettings
+    ---@realm client
+    function ttt2pms.SetPlayerSettings(settings)
+        playerSettings = settings
+        ttt2pms.SyncPlayerSettings()
+    end
+
+    net.ReveiveStream("TTT2PMS_SyncPlayerSettings", function(plySettings)
+        playerSettings = plySettings
+    end)
+end
