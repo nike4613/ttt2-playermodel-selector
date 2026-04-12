10138820a0f8b910d3b1f9105cddf974bf42008e
DaNike <danike@cirr.com>
2026-04-12 03:19:36 -0500

    Fix DDragParent trash scaling rendering issues

--- lua/ttt2pms/cl_vgui/ddragparent.lua
+++ lua/ttt2pms/cl_vgui/ddragparent.lua
@@ -92,6 +92,13 @@ local function GetZoneOrDefault(self, zone)
     return zone or (0.25 * self:GetTall())
 end
 
+---The number of seconds it takes to show or hide the trash area.
+local C_TrashShowTime = 0.5
+---The number of seconds it takes to grow or shrink the dragged panel when hovering the trash area.
+local C_GrowShrinkTime = 0.25
+---The factor by which elements shrink when held over the trash area.
+local C_ShrinkFactor = 0.6
+
 ---
 ---@param draggable DDragParent_Draggable
 function DDragParent_TTT2PMS:BeginDrag(draggable)
@@ -143,11 +150,82 @@ function DDragParent_TTT2PMS:BeginDrag(draggable)
     if draggable.trash then
         self.pnlTrash:SetVisible(true)
         self.pnlTrash:SetTall(self.nTrashHeight)
-        self.pnlTrash:Dock(TOP)
+        self.pnlTrash:SetWide(self:GetWide())
         self.pnlTrash:SetPos(0, -self.nTrashHeight)
         self.pnlTrash:SetAnimationEnabled(true)
-        self.pnlTrash:MoveTo(0, 0, 0.5, 0, 0.5)
+        self.pnlTrash:MoveTo(0, 0, C_TrashShowTime, 0, 0.5)
     end
+end
+
+---
+---@param anim AnimationData
+---@param panel Panel
+---@param t number
+local function MoveThinkBasic(anim, panel, t)
+    if !anim.StartPos then
+        anim.StartPos = Vector(panel:GetX(), panel:GetY(), 0)
+    end
+    local pos = LerpVector(t, anim.StartPos, anim.Pos)
+    panel:SetPos(pos.x, pos.y)
+end
+
+---@class RelTargetAnimData : AnimationData
+---@field RelOffs? Vector
+---@field TargetAnim? AnimationData
+---@field RelPanel? Panel should be set only when this anim is done and the panel's position needs
+---to be updated directly
+
+---
+---@param anim RelTargetAnimData
+---@param panel Panel
+---@param t number
+local function MoveThinkRelTarget(anim, panel, t)
+    if !anim.StartPos then
+        anim.StartPos = Vector(panel:GetX(), panel:GetY(), 0)
+    end
+    local pos = LerpVector(t, anim.StartPos, anim.Pos)
+    panel:SetPos(pos.x, pos.y)
+
+    if anim.RelOffs then
+        local relPos = pos + anim.RelOffs
+        if anim.TargetAnim then
+            anim.TargetAnim.Pos = relPos
+        end
+        if anim.RelPanel then
+            anim.RelPanel:SetPos(relPos.x, relPos.y)
+        end
+    end
+end
+
+---
+---@param rta RelTargetAnimData
+---@param anim AnimationData
+local function DelayAnimEndByRelTargetAnim(rta, anim)
+    local animOnEnd = anim.OnEnd
+    local rtaOnEnd = rta.OnEnd
+    local endCounter = 0
+    local animPanel, rtaPanel
+
+    local function MaybeInvokeAnimEnded()
+        endCounter = endCounter + 1
+        if endCounter == 2 then
+            animOnEnd(anim, animPanel)
+            rtaOnEnd(rta, rtaPanel)
+        end
+    end
+
+    anim.OnEnd = function(_, panel)
+        rta.RelPanel = panel
+        animPanel = panel
+        MaybeInvokeAnimEnded()
+    end
+
+    rta.OnEnd = function(_, panel)
+        rtaPanel = panel
+        MaybeInvokeAnimEnded()
+    end
+
+    rta.TargetAnim = anim
 end
 
 function DDragParent_TTT2PMS:EndDrag()
@@ -188,29 +266,59 @@ function DDragParent_TTT2PMS:EndDrag()
 
     -- hide the trash panel
     if self.draggable.trash then
-        self.pnlTrash:MoveTo(0, -self.nTrashHeight, 0.5, 0, 1, function(_, pnl)
+        ---@type RelTargetAnimData
+        local tranim = self.pnlTrash:NewAnimation(C_TrashShowTime, 0, 2, function(_, pnl)
             pnl:SetVisible(false)
             pnl:SetTall(0)
             self:SetZPos(-32768)
         end)
+        tranim.Pos = Vector(0, -self.nTrashHeight)
+        tranim.Think = MoveThinkRelTarget
+
         if self.dragState.trashHovered then
             -- need to animate the dragged panel too
             local x, y = self.draggable.panel:GetPos()
-            self.draggable.panel:MoveTo(
-                x,
-                y - self:GetTall(),
-                0.5 * (self:GetTall() / self.nTrashHeight), -- this dance makes sure that this anim moves at the same rate as the above trash anim
-                0,
-                1,
-                function(_, pnl)
+            local w, h = self.draggable.panel:GetSize()
+            local W, H = self.pnlTrash:GetSize()
+
+            -- this panel will be moved to a position relative to the trash. It will be maintained
+            -- by the MoveThinkRelTarget animation above. Thus, we need the actual position delta.
+            -- We want to place this panel in the center of the trash, so that's what we actually
+            -- want to compute.
+            local visw = w * C_ShrinkFactor
+            local visx = (W - visw) / 2
+            local adjOffsX = (1 - C_ShrinkFactor) * self.trashAnim.offsX
+
+            local vish = h * C_ShrinkFactor
+            local visy = (H - vish) / 2
+
+            -- if the dragged panel is actually taller than the trash panel, (visually) then we'll
+            -- push it way above the trash panel so that it is off-screen when needed
+            if visy < 0 then
+                visy = -(vish - H + ttt2pms.cl.plyModelRowVPadding)
+            end
+
+            local adjOffsY = (1 - C_ShrinkFactor) * self.trashAnim.offsY
+
+            local tx = visx + adjOffsX
+            local ty = visy + adjOffsY
+
+            tranim.RelOffs = Vector(tx, ty)
+
+            local pnlAnim = self.draggable.panel:NewAnimation(3*C_TrashShowTime/4, 0, -1, function(_, pnl)
                     -- destroy the animation state if its still present
                     if self.trashAnim and self.trashAnim.panel == pnl then
                         self.trashAnim = nil
                     end
                     -- and destroy the panel itself
                     pnl:Remove()
-                end
-            )
+            end)
+            pnlAnim.Think = MoveThinkBasic
+            pnlAnim.StartPos = Vector(x, y)
+            pnlAnim.Pos = Vector(x, y) -- default target vector to current pos; it'll be fixed by the first think on tranim
+
+            -- this actually wires up the anims to be linked
+            DelayAnimEndByRelTargetAnim(tranim, pnlAnim)
         else
             self.trashAnim = nil
         end
@@ -220,13 +328,14 @@ function DDragParent_TTT2PMS:EndDrag()
 
     self.draggable = nil
     self.dragState = nil
-    self.trashAnim = nil
 end
 
 function DDragParent_TTT2PMS:CancelDrag()
     if not self.draggable or not self.dragState.dragging then
         return
     end
+
+    ErrorNoHaltWithStack("Drag was canceled!")
 
     self.dragState.dragging = false
     self.draggable:cancel()
@@ -260,7 +369,7 @@ function DDragParent_TTT2PMS:Think()
         return
     end
 
-    local thinkTime = UnPredictedCurTime()
+    local thinkTime = RealTime()
 
     -- if we're currently dragging, we need to update the dragged panel positioning
     --
@@ -335,9 +444,9 @@ function DDragParent_TTT2PMS:Think()
 
         local animStartTime = thinkTime
         if self.trashAnim then
-            local t = math.min((thinkTime - self.trashAnim.startTime) / 0.5, 1)
+            local t = math.min((thinkTime - self.trashAnim.startTime) / C_GrowShrinkTime, 1)
             t = 1 - t
-            animStartTime = thinkTime - (t * 0.5)
+            animStartTime = thinkTime - (t * C_GrowShrinkTime)
         end
 
         if my > 0 and mx > bx and my > by and mx - bx < bw and my - by < bh then
@@ -383,18 +492,18 @@ function DDragParent_TTT2PMS:PaintOver(w, h)
 
     local anim = self.trashAnim
 
-    local tAbs = UnPredictedCurTime() - anim.startTime
-    local t = math.min(tAbs / 0.5, 1) -- t = relative time since animation start (capped)
+    local tAbs = RealTime() - anim.startTime
+    local t = math.min(tAbs / C_GrowShrinkTime, 1) -- t = relative time since animation start (capped)
 
     -- compute the correct scale
     local scaleStart, scaleEnd
     if anim.shrink then
         -- we've started hovering (or have been hovering), so do the ease-in (forward)
         scaleStart = 1
-        scaleEnd = 0.6
+        scaleEnd = C_ShrinkFactor
     else
         -- we're NOT hovering anymore, do the ease-out (backward)
-        scaleStart = 0.6
+        scaleStart = C_ShrinkFactor
         scaleEnd = 1
     end
 
@@ -402,10 +511,6 @@ function DDragParent_TTT2PMS:PaintOver(w, h)
     local scale = Lerp(et, scaleStart, scaleEnd)
 
     -- trash is hovered, and we need to manually render the element
-    local mtx = Matrix()
-    -- NOTE: All of the matrix modification method POSTMULTIPLY, so the last "operation" happens
-    -- FIRST
-
     local pps = surface.GetPanelPaintState()
 
     local x, y = anim.panel:GetPos()
@@ -413,24 +518,21 @@ function DDragParent_TTT2PMS:PaintOver(w, h)
     local tr = Vector(pps.translate_x + x - anim.offsX, pps.translate_y + y - anim.offsY, 0)
     tr = tr - (tr * scale)
 
+    local mtx = Matrix()
     mtx:Scale(Vector(scale, scale, 1))
     mtx:SetTranslation(tr)
 
-    PrintTable({ pps, scale, tr })
-
-    -- change the scissor rect to be correct for the projected space
-    render.SetScissorRect(
-        (pps.scissor_left - tr.x) / scale,
-        (pps.scissor_top - tr.y) / scale,
-        (pps.scissor_right - tr.x) / scale,
-        (pps.scissor_bottom - tr.y) / scale,
-        true
-    )
-
-    PrintTable(surface.GetPanelPaintState())
-
     -- actually draw the matrix using the computed matrix
     cam.PushModelMatrix(mtx, true)
+
+    -- manually enable clipping for our scaled setup
+    render.SetScissorRect(
+        pps.scissor_left,
+        pps.scissor_top,
+        pps.scissor_right,
+        pps.scissor_bottom,
+        true
+    )
 
     if self:GetDebugShow() then
         local prev = DisableClipping(true)
@@ -438,15 +540,19 @@ function DDragParent_TTT2PMS:PaintOver(w, h)
         DisableClipping(prev)
     end
 
-    anim.panel:PaintManual(false)
+    -- If we don't disable clipping here, the engine does CPU-side vertex clipping which results in
+    -- very strange looking behaviors around the edges of the (projected) view area.
+    local prev = DisableClipping(true)
+    anim.panel:PaintManual(true)
+    DisableClipping(prev)
 
-    -- fix scissor rect
+    -- remove the scissor rect again
     render.SetScissorRect(
         pps.scissor_left,
         pps.scissor_top,
         pps.scissor_right,
         pps.scissor_bottom,
-        true
+        false
     )
 
     cam.PopModelMatrix()
